bcee1481292247f0ebf4c40781c18a2d0594fe40
Vasilii Zalizniak <vzalizniak31@gmail.com>
2021-10-21 22:51:29 +0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,74 +1,13 @@
 const arr = [1, 2, 3, -1, -2, -3];
 
-let isOnlyNum = true;
-
 function getArrPositiveNum(arr) {
   const newArr = [];
-  if (arr.length === 0) {
-    isOnlyNum = false;
-    return alert(`Array is empty.`);
-  }
+  if (arr.length === 0) return `Array is empty.`;
+
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 0) {
-      newArr.push(arr[i]);
-    }
+    if (arr[i] >= 0) newArr.push(arr[i]);
   }
-  if (newArr.length > 0) {
-    return newArr;
-  } else {
-    return null;
-  }
+  return newArr.length > 0 ? newArr : null;
 }
 
-const result = getArrPositiveNum(arr);
-if (isOnlyNum) {
-  alert(result);
-}
-
-/* ----------------------------------------------------------------- */
-
-/* Решение со вспомогательной функцией для проверки данных, если я правильно понял,
-   что проверять необходимо, чтобы массив содержал однотипные данные. Если
-   мною не верно понято задание из <P.S. Можно написать вспомогательную
-   функцию для проверки данных (по желанию)>, тогда можно не учитывать это дополнение. */
-
-/* const arr = [1, 2, 3, -1, -2, -3];
-
-let isOnlyNum = true;
-
-function checkIsArrayNum(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (isNaN(arr[i]) || arr[i] === "") {
-      alert(`There are not only numbers in the array.`);
-      isOnlyNum = false;
-      break;
-    }
-  }
-}
-
-function getArrPositiveNum(arr) {
-  checkIsArrayNum(arr);
-  const newArr = [];
-  if (arr.length === 0) {
-    alert(`Array is empty.`);
-    isOnlyNum = false;
-    return;
-  }
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= 0) {
-      newArr.push(arr[i]);
-    }
-  }
-  if (newArr.length > 0) {
-    return newArr;
-  } else {
-    return null;
-  }
-}
-
-const result = getArrPositiveNum(arr);
-
-if (isOnlyNum) {
-  alert(result);
-}
- */
+alert(getArrPositiveNum(arr));
